00d8e652c77108535cf2536ef3bf822d6893b015
IdrissaN <idy447@gmail.com>
2021-04-13 18:24:52 +0200

    Add config file

--- config.py
+++ config.py
@@ -3,8 +3,11 @@ class Config:
         self.FILE_PATH = "data"
         self.runs_file_name = "runs.csv"
         self.races_file_name = "races.csv"
-        self.output_file_name = "runners_stats.csv"
+        self.output_file_name = "runners_stats.csv"   # created after feature engineering
+        self.train_file_name = "train_runners.csv"    # data for training
+        self.test_file_name = "test_runners.csv"      # data for testing models
 
         self.won_label = "won"
         self.place_label = "place"
         self.model_file_name = "baseline_model.joblib"
+        self.year_test = 2004
