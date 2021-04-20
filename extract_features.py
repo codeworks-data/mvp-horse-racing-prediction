83b31bbf691238c72c3302ed4239b851212e873c
IdrissaN <idy447@gmail.com>
2021-04-20 15:35:18 +0200

    Update functions

--- extract_features.py
+++ extract_features.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from config import Config
 from functools import reduce
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
 
 def is_place(result):
     return (result <= 3)*1
@@ -14,6 +15,21 @@ def compute_time_feats(df):
     df['quarter'] = (df['month'] + 2)//3
     
     return df
+
+def compute_horse_ratings(val):
+    if val in ['40-15','40-0','40-10','40-20']:
+        return 0
+    if val in ['60-40','65-40','60-35']:
+        return 1
+    if val in ['80-60','85-60','80-55','75-55']:
+        return 2
+    if val in ['100-80','105-80','95-75','110-85','115-90','90+','90-70','95+','110-90','100+','100-75','115-95','120-95','80+','85+','105-85','95-70','110-80','120-100']:
+        return 3
+    if val in ['G']:
+        return 4
+    else:
+        return val
+
 
 def compute_race_feats(df):  
     df['horse_under_4'] = df['horse_age'].apply(lambda a: 1 if a <= 4 else 0)
@@ -32,12 +48,15 @@ def compute_horse_feats(df, group_col='venue'):
     df = df.sort_values('date')  
     df['place'] = list(map(is_place, df['result']))
     df['last_place'] =   list(map(is_place, df.groupby('horse_id')['result'].shift()))
-    df['last_draw'] = df.groupby('horse_id')['draw'].shift()    
+    df['last_place'] = df['last_place'].fillna(0)  
+    df['last_draw'] = df.groupby('horse_id')['draw'].shift().fillna(14)    
     df['horse_rest_time'] = (df['date'] - df.groupby('horse_id')['date'].shift()).dt.days
     df['horse_rest_lest14'] = (df['horse_rest_time'] <= 14)*1
     df['horse_rest_over35'] = (df['horse_rest_time'] >= 35)*1
     df['diff_declared_weight'] = df['declared_weight'] - df.groupby('horse_id')['declared_weight'].shift()
     df['diff_distance'] = df['distance'] - df.groupby('horse_id')['distance'].shift()
+    df['horse_gear'] = df['horse_gear'].apply(lambda x: 1 if x=='--' else 0)
+    df['horse_ratings'] = df['horse_ratings'].apply(compute_horse_ratings)
     
     df['horse_runs'] = df.sort_values('date').groupby(['horse_id']).cumcount()
     df['horse_wins'] = df.sort_values('date').groupby(['horse_id'])['won'].cumsum().sub(df.won)
@@ -120,9 +139,8 @@ def compute_combinaison_feats(df, owner_1, owner_2, hippo = None):
     
     df[f'places_{owner_1}_{owner_2}'] = df.sort_values(['date','race_no']).groupby([f'{owner_1}_id', f'{owner_2}_id']).place.cumsum().sub(df.place)
     df[f'ratio_place_{owner_1}_{owner_2}'] = (df[f'places_{owner_1}_{owner_2}'] / df[f'runs_{owner_1}_{owner_2}'] )
-    df[f'ratio_place_{owner_1}_{owner_2}'].loc[(~np.isfinite(df[f'ratio_place_{owner_1}_{owner_2}'])) & df[f'ratio_place_{owner_1}_{owner_2}'].notnull()] = np.nan
     df[f'ratio_place_{owner_1}_{owner_2}'] = df[f'places_{owner_1}_{owner_2}'] / df[f'runs_{owner_1}_{owner_2}']
-    df[f'first_second_{owner_2}'] = (df[f'runs_{owner_1}_{owner_2}'] <= 2).astype(int)
+    df[f'first_second_{owner_2}'] = (df[f'runs_{owner_1}_{owner_2}'] <= 1).astype(int)
     df[f'same_last_{owner_2}'] = (df.groupby([f'{owner_1}_id'])[f'{owner_2}_id'].shift() == df[f'{owner_2}_id']).astype(int)
     
     if hippo:
@@ -131,9 +149,6 @@ def compute_combinaison_feats(df, owner_1, owner_2, hippo = None):
         df[f'ratio_win_{owner_1}_{owner_2}_{hippo}'] = (df[f'wins_{owner_1}_{owner_2}_{hippo}'] / df[f'runs_{owner_1}_{owner_2}_{hippo}'] )
         df[f'places_{owner_1}_{owner_2}_{hippo}'] = df.sort_values(['date','race_no']).groupby([f'{owner_1}_id', f'{owner_2}_id', f'{hippo}']).place.cumsum().sub(df.place)
         df[f'ratio_place_{owner_1}_{owner_2}_{hippo}'] = df[f'places_{owner_1}_{owner_2}_{hippo}'] / df[f'runs_{owner_1}_{owner_2}_{hippo}']
-        
-        df[f'ratio_place_{owner_1}_{owner_2}_{hippo}'].loc[(~np.isfinite(df[f'ratio_place_{owner_1}_{owner_2}'])) 
-                                                           & df[f'ratio_place_{owner_1}_{owner_2}_{hippo}'].notnull()] = np.nan
     
     return df
 
@@ -155,7 +170,30 @@ def compute_owner_prize_feats(df, owner=None):
 def fillna(df, na_cols, na_value=0):
     for col in na_cols:
         df[col] = df[col].fillna(na_value)
+        df.replace([np.inf, -np.inf], 0, inplace=True)
+
+    df.horse_country.fillna(df.horse_country.mode()[0], inplace=True)
+    df.horse_type.fillna(df.horse_type.mode()[0], inplace=True)
+
     return df
+
+def encode_categ_column(df):
+
+    horse_type_encoder = LabelEncoder() 
+    horse_country_encoder = LabelEncoder()
+    venue_encoder =  LabelEncoder()
+
+    going_encoder = OrdinalEncoder()
+    config_encoder = OrdinalEncoder()
+
+    df['horse_type'] = horse_type_encoder.fit_transform(df['horse_type'])
+    df['horse_country'] = horse_country_encoder.fit_transform(df['horse_country'])
+    df['venue'] = venue_encoder.fit_transform(df['venue'])
+
+    df['going'] = going_encoder.fit_transform(df['going'].values.reshape(-1, 1))
+    df['config'] = config_encoder.fit_transform(df['config'].values.reshape(-1, 1))
+
+    return df 
 
 
 if __name__ == "__main__":
@@ -164,26 +202,39 @@ if __name__ == "__main__":
     df_runs = pd.read_csv(os.path.join(config.FILE_PATH, config.runs_file_name))
     df_races = pd.read_csv(os.path.join(config.FILE_PATH, config.races_file_name))
 
+    strange_draw_index = df_runs[df_runs['draw'] > 14].index
+    df_runs = df_runs.drop(strange_draw_index)
+
+    after_race_cols = 'sec_time|time|place_combination|place_dividend|win_combination|win_dividend|lengths_behind|position_sec|behind_sec'
+    df_runs = df_runs[df_runs.columns.drop(list(df_runs.filter(regex=after_race_cols)))]
+
     df_runners = pd.merge(df_races, df_runs, on='race_id', how='left')
 
     df_runners = compute_time_feats(df_runners)
     df_runners = compute_race_feats(df_runners)
+    print("Computing horse feats ...")
     df_runners = compute_horse_feats(df_runners, group_col='venue')
     df_runners = compute_horse_feats(df_runners, group_col='surface')
     df_runners = compute_horse_feats(df_runners, group_col='going')
     df_runners = compute_horse_feats(df_runners, group_col='distance')
     df_runners = compute_horse_feats(df_runners, group_col='quarter')
+    print("Computing owner feats ...")
     df_runners = compute_owner_feats(df_runners, owner='jockey')
     df_runners = compute_owner_feats(df_runners, owner='trainer')
     df_runners = compute_owner_feats(df_runners, owner='jockey', group_col='distance')
     df_runners = compute_previous_std_ranking_to_winner(df_runners, col='result')
+    print("Computing combinaison feats ...")
     df_runners = compute_combinaison_feats(df_runners, owner_1 = 'horse', owner_2 = 'jockey', hippo = 'venue')
     df_runners = compute_combinaison_feats(df_runners, owner_1 = 'horse', owner_2 = 'trainer', hippo = 'venue')
+    print("Computing owner prize ...")
     df_runners = compute_owner_prize_feats(df_runners, owner='horse')
     df_runners = compute_owner_prize_feats(df_runners, owner='trainer')
     df_runners = compute_owner_prize_feats(df_runners, owner='jockey')
 
-    na_cols = df_runners.filter(regex="ratio|runs|wins|places|prize|avg").columns.tolist()
-    df_runners = fillna(df_runners, na_cols)  
+    print("Filling NaN values ...")
+    na_cols = df_runners.filter(regex="ratio|runs|wins|places|prize|avg|diff_").columns.tolist()
+    df_runners = fillna(df_runners, na_cols)
+    print("Encoding categorical columns ...")
+    df_runners = encode_categ_column(df_runners)
 
     df_runners.to_csv(os.path.join(config.FILE_PATH, config.output_file_name), index=False)
